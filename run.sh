7acac334f82debab8053666be36eddab3ef85102
Nigel Leck <nigel@stsoftware.com.au>
2019-02-18 14:27:45 +1100

    chmod

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+#format and mount encrypted drive
+mv /home/ec2-user /root/
+mkfs -t ext4 /dev/sdb
+mount /dev/sdb /home
+mv /root/ec2-user /home/
+
+uuid=`file -Ls /dev/sdb | sed -n "s/^.*\(UUID=\S*\).*$/\1/p"`
+echo "${uuid}     /home   ext4    defaults,nofail        0       2" >> /etc/fstab
