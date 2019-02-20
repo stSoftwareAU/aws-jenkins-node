362a6c722c6b8fc06c7aff05e157aa791f1e922b
Nigel Leck <nigel@stsoftware.com.au>
2019-02-20 20:09:17 +1100

    working on.

--- run.sh
+++ run.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 set -e
-sudo yum install -y java
-#format and mount encrypted drive
-#mv /home/ec2-user /root/
-#mkfs -t ext4 /dev/sdb
-#mount /dev/sdb /home
-#mv /root/ec2-user /home/
 
-#uuid=`file -Ls /dev/sdb | sed -n "s/^.*\(UUID=\S*\).*$/\1/p"`
-#echo "${uuid}     /home   ext4    defaults,nofail        0       2" >> /etc/fstab
+sed -i -e 's/#root:.*/root: support@stsoftware.com.au/g' /etc/aliases
+
+yum update –y
+
+yum install -y awslogs ntp java-1.8.0-openjdk-devel docker
+
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+chkconfig ntpd on
+
+# Set up logs
+sed --in-place -E "s/( *region *=)(.*)/\1 ap-southeast-2/" /etc/awslogs/awscli.conf
+
+echo "[general]" > /etc/awslogs/awslogs.conf
+echo "state_file = /var/lib/awslogs/agent-state" >> /etc/awslogs/awslogs.conf
+echo "use_gzip_http_content_encoding=true" >> /etc/awslogs/awslogs.conf
+
+echo "" >> /etc/awslogs/awslogs.conf
+echo "[/var/log/messages]" >> /etc/awslogs/awslogs.conf
+echo "log_group_name = tp-php_/var/log/messages" >> /etc/awslogs/awslogs.conf
+echo "datetime_format = %b %d %H:%M:%S" >> /etc/awslogs/awslogs.conf
+echo "file = /var/log/messages" >> /etc/awslogs/awslogs.conf
+echo "log_stream_name = {instance_id}" >> /etc/awslogs/awslogs.conf
+
+systemctl restart awslogsd.service
