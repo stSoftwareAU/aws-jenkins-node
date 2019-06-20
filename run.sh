f65b0cf2013b2165bed884c7418ac20199408e44
LG <greenberet1945@gmail.com>
2019-06-20 14:43:00 +1000

    fix script

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set +e
 adduser jenkins
 set -e
 mkdir -p /home/jenkins
-usermod --home /home/jenkins --gid docker jenkins
+usermod --home /home/jenkins jenkins
 runuser -l jenkins /usr/bin/bash -c "/usr/bin/aws configure set default.region ap-southeast-2"
 cp -a /home/ec2-user/.ssh /home/jenkins/
 chown -R jenkins:docker /home/jenkins
@@ -16,6 +16,8 @@ yum update –y
 amazon-linux-extras enable corretto8
 amazon-linux-extras install docker
 yum install -y awslogs ntp java-1.8.0-amazon-corretto git jq
+
+usermod --gid docker jenkins
 
 ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
 #chkconfig ntpd on
