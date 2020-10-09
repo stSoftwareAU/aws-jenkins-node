194e69c37b5c98583fd814361d0c8ee46ceaa510
Parminder Singh <preetzdomain@gmail.com>
2020-10-09 15:24:15 +1100

    new script for jenkins node with docker

---
+++ init.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+set -ex
+
+yum update –y
+yum install -y awslogs
+
+# Set up logs
+sed --in-place -E "s/( *region *=)(.*)/\1 ap-southeast-2/" /etc/awslogs/awscli.conf
+
+echo "[general]" > /etc/awslogs/awslogs.conf
+echo "state_file = /var/lib/awslogs/agent-state" >> /etc/awslogs/awslogs.conf
+echo "use_gzip_http_content_encoding=true" >> /etc/awslogs/awslogs.conf
+
+echo "" >> /etc/awslogs/awslogs.conf
+echo "[/var/log/messages]" >> /etc/awslogs/awslogs.conf
+echo "log_group_name = messages_/var/log/messages" >> /etc/awslogs/awslogs.conf
+echo "datetime_format = %b %d %H:%M:%S" >> /etc/awslogs/awslogs.conf
+echo "file = /var/log/messages" >> /etc/awslogs/awslogs.conf
+echo "log_stream_name = {instance_id}" >> /etc/awslogs/awslogs.conf
+
+echo "" >> /etc/awslogs/awslogs.conf
+echo "[/var/log/cloud-init-output.log]" >> /etc/awslogs/awslogs.conf
+echo "log_group_name = cloud-init-output_/var/log/cloud-init-output.log" >> /etc/awslogs/awslogs.conf
+echo "datetime_format = %b %d %H:%M:%S" >> /etc/awslogs/awslogs.conf
+echo "file = /var/log/cloud-init-output.log" >> /etc/awslogs/awslogs.conf
+echo "log_stream_name = {instance_id}" >> /etc/awslogs/awslogs.conf
+
+systemctl restart awslogsd.service
+
+sed -i -e 's/#root:.*/root: support@stsoftware.com.au/g' /etc/aliases
+
+set +e
+adduser -u 777 jenkins
+set -e
+mkdir -p /home/jenkins
+usermod --home /home/jenkins jenkins
+runuser -l jenkins /usr/bin/bash -c "/usr/bin/aws configure set default.region ap-southeast-2"
+cp -a /home/ec2-user/.ssh /home/jenkins/
+chown -R jenkins /home/jenkins
+
+amazon-linux-extras install docker
+usermod --gid docker jenkins
+chown -R jenkins:docker /home/jenkins
+systemctl start docker.service
+
+yum install -y jq
+
+# setup ssh key
+mkdir -p /home/jenkins/.ssh
+#retry 5 times to see if we can get the permission, if retry doesn't work, we still can shutdown the instance if no permission
+for i in 1 2 3 4 5; do aws secretsmanager list-secrets --region ap-southeast-2 && break || sleep 15; done
+
+secret_JS=$(aws secretsmanager get-secret-value --secret-id "common_secrets" --region ap-southeast-2)
+key_pairs_JS=$(jq -r '.SecretString' <<< "${secret_JS}")
+private_key_64=$(jq -r '.github_id_rsa' <<< "${key_pairs_JS}")
+echo "${private_key_64}" | base64 -i --decode > /home/jenkins/.ssh/id_rsa
+public_key_64=$(jq -r '.github_id_rsa_pub' <<< "${key_pairs_JS}")
+echo "${public_key_64}" | base64 -i --decode > /home/jenkins/.ssh/id_rsa.pub
+
+known_hosts=$(jq -r '.github_known_hosts' <<< "${key_pairs_JS}")
+echo "${known_hosts}" >> /home/jenkins/.ssh/known_hosts
+
+chown -R jenkins:docker /home/jenkins/.ssh
+chmod 600 /home/jenkins/.ssh/*
+
+yum install -y ntp git
+
+su - jenkins -c 'git config --global user.email "service@stsoftware.com.au"'
+su - jenkins -c 'git config --global user.name "AWS Jenkins"'
+
+ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
+#chkconfig ntpd on
+systemctl enable ntpd.service
+
+set +e
+cat /home/jenkins/.gitconfig
+set -e
+
+systemctl status docker
+
+systemctl restart sshd
