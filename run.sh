ca921ad006c4e49644003870edef1342b6972356
Harry Shaw <harry@stsoftware.com.au>
2021-08-04 11:46:27 +1000

    added dont fail for jkd

--- run.sh
+++ run.sh
@@ -107,7 +107,9 @@ yum install -y ntp maven git aspell
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.rpm --quiet
 
+set +e
 yum install -y jdk-8u141-linux-x64.rpm
+set -e
 
 #install Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
