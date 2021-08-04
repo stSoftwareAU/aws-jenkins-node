eeeeb608ae27d9e3d68e9842ce81d5f4229ea85a
Harry Shaw <harry@stsoftware.com.au>
2021-08-04 12:53:15 +1000

    added dont fail for jkd

--- run.sh
+++ run.sh
@@ -221,7 +221,7 @@ EOF
 rm -f /usr/bin/phantomjs
 rm -f /usr/local/phantomjs
 mkdir -p /usr/local/phantomjs
-wget -O- "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" | tar -jx --directory /usr/local/phantomjs --strip-components 1 --quiet
+wget -O- "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" | tar -jx --directory /usr/local/phantomjs --strip-components 1
 ln -s /usr/local/phantomjs/bin/phantomjs /usr/bin/phantomjs
 
 # moved to run-firefox.sh
