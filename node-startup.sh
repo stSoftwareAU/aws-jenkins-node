c055b75d8e136f4d94d14dd2218a5b13a8b52a33
Lei Gao <greenberet1945@gmail.com>
2021-08-20 13:57:43 +1000

    added -x flag

--- node-startup.sh
+++ node-startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script is auto pulled and run when a node is started for jenkins. 
 
-set -e
+set -ex
+echo "starting to run node-startup.sh..."
 sudo systemctl start docker.service
