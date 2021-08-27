c63a86f25061a05385b844020586ea40a325d388
Parminder Singh <preetzdomain@gmail.com>
2021-08-27 12:25:26 +1000

    start sshd and awslogs services

--- node-startup.sh
+++ node-startup.sh
@@ -3,4 +3,9 @@
 
 set -ex
 echo "starting to run node-startup.sh..."
+
 sudo systemctl start docker.service
+sudo systemctl start awslogsd.service
+sudo systemctl start sshd
+
+echo "finished running node-startup.sh"
