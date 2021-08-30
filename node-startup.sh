b6f08b14a469b3183412f8582e506c26c1fce85c
Parminder Singh <preetzdomain@gmail.com>
2021-08-31 08:08:05 +1000

    use restart to start or restart the services

--- node-startup.sh
+++ node-startup.sh
@@ -4,12 +4,12 @@
 set -ex
 echo "starting to run node-startup.sh..."
 
-sudo systemctl start docker.service
-sudo systemctl start awslogsd.service
+sudo systemctl restart docker.service
+sudo systemctl restart awslogsd.service
 
 ## NOTE ##
 ## in the file '/etc/cloud/cloud.cfg' when property 'ssh_pwauth' is set to 'false', on system reboot 'PasswordAuthentication' automatically sets to 'no'.
 sed -i "/^[^#]*PasswordAuthentication/c\PasswordAuthentication yes" /etc/ssh/sshd_config
-sudo systemctl start sshd
+sudo systemctl restart sshd
 
 echo "finished running node-startup.sh"
