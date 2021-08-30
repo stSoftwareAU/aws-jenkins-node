75a0381b9f77b33f94ff8734c7c63a281056cfeb
Parminder Singh <preetzdomain@gmail.com>
2021-08-30 14:48:48 +1000

    fixes for updating the PasswordAuthentication to yes

--- node-startup.sh
+++ node-startup.sh
@@ -6,6 +6,10 @@ echo "starting to run node-startup.sh..."
 
 sudo systemctl start docker.service
 sudo systemctl start awslogsd.service
+
+## NOTE ##
+## in the file '/etc/cloud/cloud.cfg' when property 'ssh_pwauth' is set to 'false', on system reboot 'PasswordAuthentication' automatically sets to 'no'.
+sed -i "/^[^#]*PasswordAuthentication/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo systemctl start sshd
 
 echo "finished running node-startup.sh"
--- run.sh
+++ run.sh
@@ -195,9 +195,6 @@ mkdir -p /data/${user}/upload
 chown -R root:sftp_users /data/${user}
 chown -R ${user}:sftp_users /data/${user}/upload
 
-set +e
-cat ${sshd_config}
-set -e
 
 tab=$'\t'
 cat <<EOF >>${sshd_config}
@@ -212,10 +209,6 @@ ${tab}ForceCommand internal-sftp -d /upload
 ${tab}PasswordAuthentication yes
 ## END_SFTP_CONFIG ##
 EOF
-
-set +e
-cat ${sshd_config}
-set -e
 
 
 #start docker
@@ -245,10 +238,6 @@ set +e
 cat /home/jenkins/.gitconfig
 set -e
 
-set +e
-cat ${sshd_config}
-set -e
-
 
 set +e
 bash SetupAWS.sh
@@ -256,10 +245,6 @@ bash SetupAWS.sh
 systemctl status docker
 
 systemctl restart sshd
-
-set +e
-cat ${sshd_config}
-set -e
 
 
 set -e
